1f588eb0a26ce92b0710b2d6b530c34454d1928f
unknown <ebaadraheem19@gmail.com>
2025-12-10 15:03:11 +0500

    updated article.ts of frontend

--- frontend/src/api/article.ts
+++ frontend/src/api/article.ts
@@ -1,28 +1,28 @@
 import type { Article } from "../components/Func_Types";
 
-const API_BASE_URL: string = '/api';
+const API_BASE_URL: string = window.location.origin + "/api";
 
 export async function fetchArticles(): Promise<Article[]> {
-    const response = await fetch(`${API_BASE_URL}/articles`); 
-    
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data: Article[] = await response.json();
-    return data;
+  const response = await fetch(`${API_BASE_URL}/articles`);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data: Article[] = await response.json();
+  return data;
 }
 
 export async function fetchArticleById(id: string): Promise<Article | null> {
-    const response = await fetch(`${API_BASE_URL}/articles/${id}`);
+  const response = await fetch(`${API_BASE_URL}/articles/${id}`);
 
-    if (response.status === 404) {
-        return null; 
-    }
-    
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  if (response.status === 404) {
+    return null;
+  }
 
-    const data: Article = await response.json();
-    return data;
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data: Article = await response.json();
+  return data;
 }
